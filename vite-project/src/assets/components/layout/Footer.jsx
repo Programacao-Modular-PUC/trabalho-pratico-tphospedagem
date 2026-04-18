1576c0f187a464479b3296d1bd7e5da78eaae31d
luiz <lduducamposs@gmail.com>
2026-04-18 13:19:38 -0300

    css da navbar

--- vite-project/src/assets/components/layout/Footer.jsx
+++ vite-project/src/assets/components/layout/Footer.jsx
@@ -1,7 +1,11 @@
+import styles from './Footer.module.css'
+
 function Footer(){
 
     return (
-        <p>aa</p>
+        <Footer>
+            <p><span>Hospedagem</span> &copy; </p>
+        </Footer>
     )
 }
 
--- vite-project/src/assets/components/layout/Navbar.jsx
+++ vite-project/src/assets/components/layout/Navbar.jsx
@@ -5,27 +5,27 @@ import logo from '../img/logohospedagem.png'
 
 function Navbar(){
     return(
-        <nav>
+        <nav className={styles.navbar}>
         <Container>
         <Link to="/">
             <img src={logo} alt="hospedagem" />
         </Link>
 
-        <ul>
-            <li>
+        <ul className={styles.list}>
+            <li className={styles.item}>
                 <Link to="/">Home</Link>
         
             </li>
-            <li>
+            <li className={styles.item}>
                 <Link to="/residencias">Residencias</Link>
             </li>
-            <li>
+            <li className={styles.item}>
                 <Link to="/quartos">Quartos</Link>
             </li>
-            <li>
+            <li className={styles.item}>
                 <Link to="/clientes">Cliente</Link>
             </li>
-            <li>
+            <li className={styles.item}>
                 <Link to="/reserva">Reserva</Link>
             </li>
         </ul>
