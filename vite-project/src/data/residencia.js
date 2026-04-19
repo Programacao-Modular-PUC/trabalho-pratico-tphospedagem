c54ffb6765e0db7d6e41237ed662e234c8e8c5ee
luiz <lduducamposs@gmail.com>
2026-04-18 23:54:07 -0300

    adicionando img nas residencias

--- vite-project/src/data/residencia.js
+++ vite-project/src/data/residencia.js
@@ -1,21 +1,24 @@
+import casa1 from '../assets/components/img/casa1.jpg'
+import casa2 from '../assets/components/img/casa2.jpg'
+import casa3 from '../assets/components/img/casa3.jpg'
 const residencias = [
   {
     id: 1,
-    imagem: "/imagens/casa1.jpg",
+    imagem: casa1,
     endereco: "Rua das Flores, 123",
     bairro: "Centro",
     telefone: "(31) 98395-9523"
   },
   {
     id: 2,
-    imagem: "/imagens/casa1.jpg",
+    imagem: casa2,
     endereco: "Rua das Alamedas, 845",
     bairro: "Barreiro",
     telefone: "(31) 95439-3659"
   },
   {
     id: 3,
-    imagem: "/imagens/casa1.jpg",
+    imagem: casa3,
     endereco: "Rua das Lindas, 23",
     bairro: "Industrial",
     telefone: "(31) 99485-89734"
